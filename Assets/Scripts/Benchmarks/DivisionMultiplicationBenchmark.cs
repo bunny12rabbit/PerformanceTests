420ee4b6f0fe2d5b49c5d30000ed39445017d657
Andrey Romanenko <a.romanenko@gamythic.com>
2022-12-29 18:10:28 +0300

    Добавил тест на деление вектора и числа на 2 или умножение на 0.5f с кастом к инту и без;

---
+++ Assets/Scripts/Benchmarks/DivisionMultiplicationBenchmark.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Benchmarks
+{
+    public class DivisionMultiplicationBenchmark : BenchmarkBase
+    {
+        protected override void Init()
+        {
+            BenchmarkName = "Division vs Multiplication";
+
+            benchmarks.Add(new BenchmarkData("Vector divide by 2", () =>
+            {
+                var a = Vector2.one / 2;
+            }));
+
+            benchmarks.Add(new BenchmarkData("Vector multiply by 0.5f", () =>
+            {
+                var a = Vector2.one * 0.5f;
+            }));
+
+            benchmarks.Add(new BenchmarkData("number divide by 2", () =>
+            {
+                var a = 120 / 2;
+            }));
+
+            benchmarks.Add(new BenchmarkData("number multiply by 0.5f (float result)", () =>
+            {
+                var a = 120 * 0.5f;
+            }));
+
+            benchmarks.Add(new BenchmarkData("number multiply by 0.5f (cast to int)", () =>
+            {
+                var a = (int) (120 * 0.5f);
+            }));
+        }
+    }
+}
